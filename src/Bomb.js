ad33cad518dde98799afce81fe6883d2f7bb8a4c
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-19 13:39:33 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,15 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  render() {
+    return (
+      <div id="matrix">
+        
+      </div>
+    )
+  }
+  
+}
+
+
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,19 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+   constructor(props) {
+  super()
+  this.state = {
+    currentSlideIndex  : 0,
+  } 
+}
+  render() {
+    return (
+      <div id="matrix">
+        
+      </div>
+    )
+  }
+  
+}
