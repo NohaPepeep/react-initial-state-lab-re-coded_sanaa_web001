f827dc5d191b3f7d38f78754b7e48503145a5845
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-19 13:46:57 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,6 +2,12 @@
 import React, { Component } from 'react';
 
 export default class Bomb extends Component {
+  constructor(props) {
+  super()
+  this.state = {
+    currentSlideIndex  : 0,
+  } 
+}
   render() {
     return (
       <div id="matrix">
